ecd269ce18a28cd2398823cad487e8e36b857799
KHADIJA NAZ <67429399+KhadijaNaz@users.noreply.github.com>
2022-05-29 20:31:18 +0500

    Added urls.py in App
    > added urls.py in App
    > Home, About

--- Myweb/Mywebapp/views.py
+++ Myweb/Mywebapp/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# Create your views here.
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse("This is Home page.")
+
+def about(request):
+    return HttpResponse("This is About page.")    
