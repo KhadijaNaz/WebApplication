4cd8bb8279d7ac00f43e0fb28e2cce2f37f0d6bf
KHADIJA NAZ <67429399+KhadijaNaz@users.noreply.github.com>
2022-07-22 12:05:30 +0500

    Templates added

--- Myweb/Mywebapp/views.py
+++ Myweb/Mywebapp/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("This is Home page.")
+    return render(request, 'index.html')
+    #return HttpResponse("This is Home page.")
 
 def about(request):
     return HttpResponse("This is About page.") 
